4b5bf2899e9278519d24ab6aa88e1e9581818e91
Ryanshinta <ryanouyangyunru@gmail.com>
2022-02-13 23:07:29 +0800

    CRUD for item

---
+++ src/main/dao/itemMapper.java
@@ -0,0 +1,18 @@
+package main.dao;
+
+import main.entity.item;
+
+/**
+ * @author Ryan Ouyang
+ */
+public interface itemMapper {
+
+    boolean newItem(item i);
+
+    boolean deleteById(Long Id);
+
+    boolean updateItemById(Long Id);
+
+    item searchById(long Id);
+
+}
---
+++ src/main/entity/item.java
@@ -0,0 +1,62 @@
+package main.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Ryan Ouyang
+ * Basic entity for item
+ */
+public class item {
+    private long itemID;
+    private String itemName;
+    private String itemDesc;
+    private BigDecimal itemPrice;
+
+    public item(long itemID, String itemName, String itemDesc, BigDecimal itemPrice) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemDesc = itemDesc;
+        this.itemPrice = itemPrice;
+    }
+
+    public long getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(long itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    public void setItemDesc(String itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "item" +
+                "itemID='" + itemID + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", itemDesc='" + itemDesc + '\'' +
+                ", itemPrice=" + itemPrice ;
+    }
+}
