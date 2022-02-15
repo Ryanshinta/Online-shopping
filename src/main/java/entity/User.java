6dd19fbef9118650a8aebbcd67cd2fb293b25640
Yongen1019 <88510369+Yongen1019@users.noreply.github.com>
2022-02-15 18:51:03 +0800

    Update User.java

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String password;
@@ -24,10 +26,41 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public String toString() {
         return "username : " + username + ", password : " + password;
 
+    }
+    
+    public String toString2() {
+        return String.format("%-20s %-20s", username, password);
     }
 }
