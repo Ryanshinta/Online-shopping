7412daf526fff423d6df16d435e69d2bba127d02
Ryanshinta <ryanouyangyunru@gmail.com>
2022-02-15 12:30:07 +0800

    Test CRUD function for Item

---
+++ src/test/java/testItemCRUD.java
@@ -0,0 +1,34 @@
+
+import entity.item;
+import service.ItemService;
+
+import java.math.BigDecimal;
+
+public class testItemCRUD {
+    public static void main(String[] args){
+        ItemService itemService = new ItemService();
+        item Item0001 = new item(0001,"TestProduct","This is a test Product", BigDecimal.valueOf(99.99));
+        item Item0002 = new item(0002,"TestProduct2","This is a test Product2", BigDecimal.valueOf(99.92));
+        // new item
+        itemService.newItem(Item0001);
+        itemService.newItem(Item0002);
+        // search itme
+        item searchItem1 = itemService.searchById(0001);
+        item searchItem2 = itemService.searchById(0002);
+        System.out.println(searchItem1.toString()+"\n"+searchItem2.toString());
+        // update item
+        Item0002.setItemPrice(BigDecimal.valueOf(22.22));
+        itemService.updateItemById(Item0002.getItemID(),Item0002);
+        item searchItem3 = itemService.searchById(0002);
+        System.out.println(searchItem3.toString());
+        // delete item
+        itemService.deleteById(0002L);
+        item deleteItem = itemService.searchById(0002);
+        if (deleteItem == null){
+            System.out.println("ID 0002 already deleted");
+        }else {
+            System.out.println(deleteItem.toString());
+        }
+
+    }
+}
