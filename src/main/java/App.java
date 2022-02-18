90942bf06d3f55324bead2f89f82e3a13a3a7d84
geeyong1115 <53674303+geeyong1115@users.noreply.github.com>
2022-02-18 11:23:14 +0800

    remove reset password from geeyong

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,23 +2,12 @@ import static util.textColor.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import controller.UserController;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1. reset password");
-        int choice = sc.nextInt();
 
-        if (choice == 1) {
-            UserController user = new UserController();
-            user.resetPassword();
-
-            // ArrayList<String> test = UserController.usernames;
-            // System.out.println(test);
-
-        }
         // System.out.println(TEXT_RED + "This test is red!" + TEXT_RESET);
     }
 }
