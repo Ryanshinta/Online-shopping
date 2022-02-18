008c44cf798c9a70089dd3f1f261d499a92ec8b8
geeyong1115 <53674303+geeyong1115@users.noreply.github.com>
2022-02-18 11:21:14 +0800

    .

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,64 +1,47 @@
 package controller;
 
 import java.util.Scanner;
-import entity.User;
 import java.util.ArrayList;
-import entity.User;
-import dao.userMapper;
-import service.UserService;
-import java.util.Scanner;
-import static util.textColor.*;
 
 public class UserController {
 
-    private userMapper<User> userList = new UserService<>();
+    public static ArrayList<String> usernames = new ArrayList<String>();
+    public static ArrayList<String> passwords = new ArrayList<String>();
+    // public static ArrayList<User> test = new ArrayList<User>();
 
-    public void hardCodeUser() {
-        userList.add(new User("abc", "123"));
-        userList.add(new User("def", "456"));
-        userList.add(new User("ghi", "789"));
-        userList.add(new User("jkl", "012"));
+    // interface User {
+    // String username = "a";
+    // String password = "123";
+
+    // }
+
+    public void hardCode() {
+        usernames.add("a");
+        usernames.add("abc");
+        usernames.add("c");
+
+        passwords.add("123");
+        passwords.add("456");
+        passwords.add("789");
     }
 
     public void resetPassword() {
-        int n = 0;
-
+        hardCode();
         System.out.print("Enter your username: ");
         Scanner sc = new Scanner(System.in);
         String username = sc.nextLine();
-        // usernames.add(username);
-        for (int i = 1; i <= userList.getNumberOfEntries(); i++) {
-            String mn = userList.getEntry(i).getUsername();
-            if (mn.equals(username)) {
-                n++;
-            }
-        }
-        if (n != 0) {
-            String password;
-            String confirmpassword;
-            do {
-                System.out.print("Enter new password: ");
-                password = sc.nextLine();
-                while ("".equals(password)) {
-                    System.out.println(TEXT_RED + "Error: Password cannot be empty.\n" + TEXT_RESET);
-                    System.out.print("Password         : ");
-                    password = sc.nextLine();
-                }
-                System.out.print("Confirm new password : ");
-                confirmpassword = sc.nextLine();
-                if (!(password).equals(confirmpassword)) {
-                    System.out.println(
-                            TEXT_RED + "Error: password is different, pls reenter password again.\n"
-                                    + TEXT_RESET);
-                }
-            } while (!(password).equals(confirmpassword));
-            User s = new User(username, password);
-            if (userList.add(s)) {
-                System.out.println(
-                        TEXT_GREEN + "\nPassword for [" + username + "] has been reset successfully." + TEXT_RESET);
-            }
+
+        if (usernames.contains(username)) {
+            System.out.print("Enter your new password: ");
+            String password = sc.nextLine();
+
+            passwords.add(password);
+            System.out.println("new password reset successfully");
+
+            // System.out.print("username after changes:" + usernames.toString());
+            // System.out.print("password after changes:" + passwords.toString());
         } else {
-            System.out.println(TEXT_RED + "\nError: [" + username + "] cant find." + TEXT_RESET);
+            System.out.print("username not found ");
 
         }
 
