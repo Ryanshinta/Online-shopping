70ed84ed097e7f9286bd1e6099d9c7f80e3f2388
Ryanshinta <ryanouyangyunru@gmail.com>
2022-02-24 23:18:55 +0800

    Update Item Controller update

--- src/main/java/controller/itemController.java
+++ src/main/java/controller/itemController.java
@@ -19,7 +19,6 @@ public class itemController {
     ItemService itemService = new ItemService();
 
 
-
     public void menu() {
         while (true) {
             int selection = 0;
@@ -89,7 +88,7 @@ public class itemController {
                     itemService.newItem(new item(itemName, itemDesc, BigDecimal.valueOf(Double.valueOf(itemPrice))));
                     System.out.println(TEXT_GREEN + "Item add success" + TEXT_RESET);
                     break;
-                } else if (i.equals('n')|| i.equals("N")) {
+                } else if (i.equals('n') || i.equals("N")) {
                     System.out.println("discard.");
                     break;
                 }
@@ -113,25 +112,25 @@ public class itemController {
             System.out.println("Enter your ID that you want to delete");
             String target = scanner.nextLine();
             item i = itemService.searchById(Integer.valueOf(target));
-            if (i == null){
+            if (i == null) {
                 System.out.println("No item found");
-            }else {
+            } else {
                 System.out.println(
-                        "Item ID :"+i.getItemID()+ "\n" +
-                                "Item Name: "+i.getItemName()+"\n"+
-                                "Item Desc: "+i.getItemDesc()+"\n"+
-                                "Item Price: RM"+i.getItemPrice()+"\n");
+                        "Item ID :" + i.getItemID() + "\n" +
+                                "Item Name: " + i.getItemName() + "\n" +
+                                "Item Desc: " + i.getItemDesc() + "\n" +
+                                "Item Price: RM" + i.getItemPrice() + "\n");
                 System.out.println("conform delete? (Yea = y, No = n)");
                 String n = scanner.nextLine();
                 if (n.equals("y") || n.equals("Y")) {
                     itemService.deleteById(Integer.valueOf(target));
                     System.out.println(TEXT_GREEN + "Item delete success" + TEXT_RESET);
-                } else if (n.equals('n')|| n.equals("N")) {
+                } else if (n.equals('n') || n.equals("N")) {
                     System.out.println("discard.");
                 }
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(TEXT_RED + e + TEXT_RESET);
             System.out.println(TEXT_RED + "invalid input Please enter again." + TEXT_RESET);
         }
@@ -145,33 +144,92 @@ public class itemController {
             System.out.println("Enter your ID that you want to search");
             String target = scanner.nextLine();
             item i = itemService.searchById(Integer.valueOf(target));
-            if (i == null){
+            if (i == null) {
                 System.out.println("No item found");
-            }else {
+            } else {
                 System.out.println(
-                        "Item ID :"+i.getItemID()+ "\n" +
-                                "Item Name: "+i.getItemName()+"\n"+
-                                "Item Desc: "+i.getItemDesc()+"\n"+
-                                "Item Price: RM"+i.getItemPrice()+"\n");
+                        "Item ID :" + i.getItemID() + "\n" +
+                                "Item Name: " + i.getItemName() + "\n" +
+                                "Item Desc: " + i.getItemDesc() + "\n" +
+                                "Item Price: RM" + i.getItemPrice() + "\n");
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(TEXT_RED + e + TEXT_RESET);
             System.out.println(TEXT_RED + "invalid input Please enter again." + TEXT_RESET);
         }
     }
 
     public void UpdateItem() {
+        Scanner scanner = new Scanner(System.in);
 
+        try {
+            System.out.println("Enter your ID that you want to Update");
+            String target = scanner.nextLine();
+            item i = itemService.searchById(Integer.valueOf(target));
+            if (i == null) {
+                System.out.println("No item found");
+            } else {
+                System.out.println(
+                        "0. Item ID :" + i.getItemID() + "\n" +
+                                "1. Item Name: " + i.getItemName() + "\n" +
+                                "2. Item Desc: " + i.getItemDesc() + "\n" +
+                                "3. Item Price: RM" + i.getItemPrice() + "\n");
+                System.out.println("Which one need modify(ID cannot modify)");
+                String n = scanner.nextLine();
+                switch (Integer.valueOf(n)) {
+                    case 1:
+                        System.out.println("Enter the Name for item: ");
+                        String itemName = scanner.nextLine();
+                        if (StringUtils.isAllBlank(itemName) || StringUtils.isAllEmpty(itemName)) {
+                            throw new Exception();
+                        }
+                        i.setItemName(itemName);
+                        System.out.println(TEXT_GREEN+"Update success"+TEXT_RESET);
+                        break;
+                    case 2:
+                        System.out.println("Enter the Desc for item");
+                        String itemDesc = scanner.nextLine();
+                        if (StringUtils.isAllBlank(itemDesc) || StringUtils.isAllEmpty(itemDesc)) {
+                            throw new Exception();
+                        }
+                        i.setItemDesc(itemDesc);
+                        System.out.println(TEXT_GREEN+"Update success"+TEXT_RESET);
+                        break;
+                    case 3:
+                        System.out.println("Enter the price for item");
+                        String itemPrice = scanner.nextLine();
+                        if (StringUtils.isNumeric(itemPrice) || StringUtils.isAllEmpty(itemPrice) || StringUtils.isBlank(itemPrice)) {
+                            throw new Exception();
+                        }
+                        i.setItemPrice(BigDecimal.valueOf(Double.valueOf(itemPrice)));
+                        System.out.println(TEXT_GREEN+"Update success"+TEXT_RESET);
+                        break;
+
+                    default:
+                        System.out.println(
+                                "0. Item ID :" + i.getItemID() + "\n" +
+                                        "1. Item Name: " + i.getItemName() + "\n" +
+                                        "2. Item Desc: " + i.getItemDesc() + "\n" +
+                                        "3. Item Price: RM" + i.getItemPrice() + "\n");
+                        break;
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(TEXT_RED + e + TEXT_RESET);
+            System.out.println(TEXT_RED + "invalid input Please enter again." + TEXT_RESET);
+        }
     }
 
     public void DisplayAllItem() {
-        for (item i:itemService.displayAllItem().values()) {
+        for (item i : itemService.displayAllItem().values()) {
             System.out.println(
-                    "Item ID :"+i.getItemID()+ "\n" +
-                    "Item Name: "+i.getItemName()+"\n"+
-                    "Item Desc: "+i.getItemDesc()+"\n"+
-                    "Item Price: RM"+i.getItemPrice()+"\n");
+                    "Item ID :" + i.getItemID() + "\n" +
+                            "Item Name: " + i.getItemName() + "\n" +
+                            "Item Desc: " + i.getItemDesc() + "\n" +
+                            "Item Price: RM" + i.getItemPrice() + "\n");
         }
     }
 
