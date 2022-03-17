0bb3b1f019ec53c4034ed5274a0aabff1e9c979b
geeyong1115 <53674303+geeyong1115@users.noreply.github.com>
2022-03-17 18:04:40 +0800

    add unit test for announcement

--- src/test/java/testAnnouncement.java
+++ src/test/java/testAnnouncement.java
@@ -1,13 +1,53 @@
+import entity.Announcement;
+import org.junit.Before;
+import org.junit.Test;
+import service.announcementService;
 
-import controller.announcementController;
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
 
 /**
  *
  * @author Tay Gee Yong
  */
 public class testAnnouncement {
-    public static void main(String[] args) {
-        announcementController ann = new announcementController();
-        ann.menuAnn();
+
+    static announcementService<Announcement> annList = new announcementService<>();
+    private Announcement a1;
+
+    @Before
+    public void setup() {
+        a1 = new Announcement(2222, "test case message", "2022/03/22 16:37:15");
+        annList.add(a1);
     }
+
+    @Test
+    public void testRemoveAnnouncement() {
+        boolean expResult = true;
+        boolean result = annList.remove(a1);
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testAddAnnouncement() {
+
+        boolean expResult = true;
+        annList.add(a1);
+        boolean result = false;
+
+        Iterator iterator = annList.getIterator();
+        while (iterator.hasNext()) {
+            Announcement ann = (Announcement) iterator.next();
+            while (ann.getAnnID().equals(2222)) {
+                result = true;
+                break;
+            }
+        }
+
+        assertEquals(expResult, result);
+
+    }
+
 }
